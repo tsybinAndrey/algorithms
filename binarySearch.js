2e7f6681f32a843899ec391aad9ce7593f5df9fb
Andrew Tsybin <tcandrei@bk.ru>
2019-09-22 21:43:18 +0300

    init

---
+++ binarySearch.js
@@ -0,0 +1,24 @@
+/**
+ * Binary Search
+ * @param {number|string[]} list - sorted array of values
+ * @param {number|string} searchFor - number you wanna search for
+ * @returns {number} - index of found item in array or -1 if there is no item
+ */
+exports.binarySearch = function (list, searchFor) {
+  let low = 0
+  let high = list.length - 1
+
+  while(low <= high) {
+    const mid = Math.round((low + high) / 2)
+
+    if (list[mid] === searchFor) return mid
+
+    if (list[mid] > searchFor) {
+      high = mid - 1
+    } else {
+      low = mid + 1
+    }
+  }
+
+  return -1;
+}
---
+++ index.js
@@ -0,0 +1,6 @@
+const {binarySearch} = require('./binarySearch')
+
+const array = [1, 3, 5, 7]
+const stringArray = ['a', 'b', 'c', 'd']
+
+console.log(binarySearch(stringArray, 'e'))
